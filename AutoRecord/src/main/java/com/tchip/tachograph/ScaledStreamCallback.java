e33d2b9b3a7376827c66af06289a87589105ef85
JiangShen <iplaycloud@gmail.com>
2017-02-20 20:57:51 +0800

    edit pacage name

--- AutoRecord/src/main/java/com/tchip/tachograph/ScaledStreamCallback.java
+++
@@ -1,6 +0,0 @@
-package com.tchip.tachograph;
-
-
-public interface ScaledStreamCallback {
-    public void onScaledStream(byte[] data , int width , int height);
-}
---
+++ AutoRecord/src/main/java/com/xctx/tachograph/ScaledStreamCallback.java
@@ -0,0 +1,6 @@
+package com.xctx.tachograph;
+
+
+public interface ScaledStreamCallback {
+    public void onScaledStream(byte[] data , int width , int height);
+}
